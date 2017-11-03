c7b18a221931e74c7b34b908fe7f8471630ec650
cadenzasong <cadenzasong@me.com>
2017-11-02 22:46:53 -0700

    fix pose_cb and waypoints_cb in WaypointUpdater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -4,8 +4,11 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 from std_msgs.msg import Int32
+import sys
 
 import math
+
+ONE_MPH = 0.44704
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -39,49 +42,40 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
-        self.current_pose = None
-        self.waypoints = None
-        
+        self.base_waypoints = None
+
         rospy.spin()
-  
-    def closest_wp (self, waypoints):
-        """#Measure the closest waypoint ahead of the car
-        min_dist = 10000
-        dist = 0
+
+    def closest_wp(self, current_pose):
+        # Measure the closest waypoint ahead of the car
+        min_dist = sys.float_info.max
+        dist = 0.0
+        closest_idx = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        #Search in all waypoints
-        for i in range(0, LOOKHEAD_WPS):
-            if (waypoints[i].pose.pose.position.x > 0):
-                #This value is in front of the car
-                dist = dl(self.current_pose.position, waypoints[i].pose.pose.position)
-            #Compare if the calculated distance is the closest distance
+        # Search in all waypoints
+        for i in range(0, len(self.base_waypoints)):
+            dist = dl(current_pose.position, self.base_waypoints[i].pose.pose.position)
+            # Compare if the calculated distance is the closest distance
             if (dist < min_dist):
                 min_dist = dist
                 #Save the index of the closest point ahead of the car
-                closest_indx=i
-        return closest_indx"""
-        return 0
-    
-    def waypoints_cb(self, waypoints):
-        """# TODO: Implement
-        self.waypoints = waypoints.waypoints
-        #Check if the waypoints are received
-        if not self.waypoints:
-            #Get the index of the closest waypoint in the waypoints
-            closest_indx = closest_wp (self.current_pose,self.waypoints) 
+                closest_idx = i
+        return (closest_idx + 1)
 
-        #Create final_wp msg that has Lane type. Fill it and then publish it
-        final_wp = Lane()
-        final_wp.header.frame_id = waypoints.header.frame_id
-        final_wp.header.stamp = rospy.get_rostime()
-        final_wp.waypoints = self.waypoints[closest_indx:closest_indx+LOOKAHEAD_WPS]
-        self.final_waypoints_pub.publish(final_wp)"""
-        pass
-    
     def pose_cb(self, msg):
-        # TODO: Implement
-        self.current_pose = msg
-        pass
+        # rospy.logwarn("pose_cb")
+        closest_idx = self.closest_wp(msg.pose)
+        final_wp = Lane()
+        final_wp.header.frame_id = msg.header.frame_id
+        final_wp.header.stamp = rospy.get_rostime()
+        final_wp.waypoints = self.base_waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        for i in range(LOOKAHEAD_WPS):
+            self.set_waypoint_velocity(final_wp.waypoints, i, 10.0 * ONE_MPH)
+        self.final_waypoints_pub.publish(final_wp)
+
+    def waypoints_cb(self, waypoints):
+        # rospy.logwarn("waypoints_cb")
+        self.base_waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
